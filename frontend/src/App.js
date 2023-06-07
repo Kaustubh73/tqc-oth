aa03c1532ea2254ded0eda5a008983db8b3e2f73
Kaustubhg73 <kaustubhg73@gmail.com>
2023-06-07 22:06:28 +0530

    Homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,40 @@
 import './App.css';
 import React from 'react'
 import Header from './Header';
+function FeaturedContent() {
+  const currContest = {
+    name: "Trio Quiz Competition 2.0",
+    duration: "June 3 - June 10, 2023",
+    instructions: "Solve a series of Rounds and gain points with each win on the board!",
+    prizes: "1st Place : Rs 500 | 2nd Place : Rs 300 | 3rd Place : Rs 200",
+  };
 
+  return (
+    <div>
+      <h2>{currContest.name}</h2>
+      <p>{currContest.duration}</p>
+      <p>{currContest.instructions}</p>
+      <p>{currContest.prizes}</p>
+      <button>Join the Contest</button>
+    </div>
+  )
+}
 function WelcomeMessage() {
   return (
+    <div>
     <div className="welcome-message">
       <h1>The Trio Treasure Hunt!</h1>
       <p>Get ready for an exciting adventure filled with puzzles, challenges, and rewards.</p>
-      <button>Start the Hunt</button>
     </div>
+    <div className="featured">
+      <FeaturedContent/>
+    </div>
+  </div>
   );
 }
 
 const user = {
-  name: 'Hedy Lamarr',
+  Username: 'Hedy Laamarr',
   imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
   imageSize: 90,
 };
@@ -23,14 +44,9 @@ function App() {
     <div className="App">
       <Header />
       <WelcomeMessage />
-      <p> {user.name} </p>
+      <p> {user.Username} </p>
       <img src = {user.imageUrl} alt={user.name}/>
-      <p> {user.name} </p>
-      <img src = {user.imageUrl} alt={user.name}/>
-      <p> {user.name} </p>
-      <img src = {user.imageUrl} alt={user.name}/>
-      <p> {user.name} </p>
-      <img src = {user.imageUrl} alt={user.name}/>
+
     </div>
   );
 }
