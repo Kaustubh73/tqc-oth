829bf7d4442e98b4e34b6fde384586a213e9de51
Kaustubhg73 <kaustubhg73@gmail.com>
2023-06-07 17:42:48 +0530

    Updates

---
+++ precache-manifest.93c30e49e170b7f4e043a496cfafaf3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "16b691410116cad0fb9f",
+    "url": "/tqc-oth/static/css/main.2f1fcae2.chunk.css"
+  },
+  {
+    "revision": "16b691410116cad0fb9f",
+    "url": "/tqc-oth/static/js/main.16b69141.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/tqc-oth/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/tqc-oth/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "864534661ed859cf1e48",
+    "url": "/tqc-oth/static/js/runtime~main.86453466.js"
+  },
+  {
+    "revision": "f75c532b922791922be64c60b8c8eebb",
+    "url": "/tqc-oth/static/media/Puzzle1.f75c532b.jpg"
+  },
+  {
+    "revision": "302b2016130a97be26c672be794334a8",
+    "url": "/tqc-oth/index.html"
+  }
+];
