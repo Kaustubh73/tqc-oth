639648af24bb729574e5c43096cf5c98203f802d
Kaustubhg73 <kaustubhg73@gmail.com>
2023-06-07 13:56:40 +0530

    react setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+function MyButton() {
+  return (
+    <button>I'm a button</button>
+  )
+}
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Trio Competition</h1>
+      <p>Hello there. <br/> How you doin? </p>
+      <img src={logo} className="App-logo "/>
+      <p> <br/></p>
+      <MyButton />
+      <p> {user.name} </p>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of' + user.name}
+        style = {{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </div>
   );
 }
