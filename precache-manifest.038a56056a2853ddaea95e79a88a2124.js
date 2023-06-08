8650d0f9876657fd92b9a00871a5510993c81443
Kaustubhg73 <kaustubhg73@gmail.com>
2023-06-08 20:04:19 +0530

    Updates

---
+++ precache-manifest.038a56056a2853ddaea95e79a88a2124.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "e14858ec72305663b5d0",
+    "url": "/tqc-oth/static/css/main.1d3985ff.chunk.css"
+  },
+  {
+    "revision": "e14858ec72305663b5d0",
+    "url": "/tqc-oth/static/js/main.e14858ec.chunk.js"
+  },
+  {
+    "revision": "9ad960da41216fce2577",
+    "url": "/tqc-oth/static/js/1.9ad960da.chunk.js"
+  },
+  {
+    "revision": "ade9d5aa0bbc3f1be8b2",
+    "url": "/tqc-oth/static/js/2.ade9d5aa.chunk.js"
+  },
+  {
+    "revision": "ac0271001d24eee0a262",
+    "url": "/tqc-oth/static/js/runtime~main.ac027100.js"
+  },
+  {
+    "revision": "f75c532b922791922be64c60b8c8eebb",
+    "url": "/tqc-oth/static/media/Puzzle1.f75c532b.jpg"
+  },
+  {
+    "revision": "345ba7b9c53f8e7f93344599a41a370b",
+    "url": "/tqc-oth/static/media/Sudoku.345ba7b9.png"
+  },
+  {
+    "revision": "44f6ff52106745692cead25db43fcc9c",
+    "url": "/tqc-oth/static/media/Question 2.44f6ff52.png"
+  },
+  {
+    "revision": "ecbda225e0272111bdcb42550e70540e",
+    "url": "/tqc-oth/static/media/Question 3.ecbda225.png"
+  },
+  {
+    "revision": "fe3547576dff9a855fe7f8f5015ecaf7",
+    "url": "/tqc-oth/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tqc-oth/precache-manifest.1262d15c724c2333aa29d9b4c3b0a21f.js"
+  "/tqc-oth/precache-manifest.038a56056a2853ddaea95e79a88a2124.js"
 );
 
 workbox.clientsClaim();
